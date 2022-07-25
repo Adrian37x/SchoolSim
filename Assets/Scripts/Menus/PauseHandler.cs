fca8522d10fb5cd5183b2245261d907d50c0a434
Adrian Habeler <adrian.habeler@gmail.com>
2022-07-25 14:56:38 +0200

    .

---
+++ Assets/Scripts/Menus/PauseHandler.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseHandler : MonoBehaviour
+{
+	public GameObject pausePanel;
+    public bool isPaused;
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+		{
+			if (isPaused)
+			{
+				Resume();
+			}
+			else
+			{
+				Pause();
+			}
+		}
+    }
+
+	public void Resume()
+	{
+		isPaused = false;
+		Time.timeScale = 1;
+		pausePanel.SetActive(false);
+	}
+
+	public void Exit()
+	{
+		Application.Quit();
+	}
+
+	private void Pause()
+	{
+		isPaused = true;
+		Time.timeScale = 0;
+		pausePanel.SetActive(true);
+	}
+}
